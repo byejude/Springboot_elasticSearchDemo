a0c36f10d9d9ac6c2e7cc029db5fb700d55f7409
byejude <edelweissasuare@163.com>
2018-01-21 11:58:12 +0800

    init project

---
+++ src/test/java/com/tulip/springbootelasticsearchdemo/SpringbootelasticsearchdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tulip.springbootelasticsearchdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootelasticsearchdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
