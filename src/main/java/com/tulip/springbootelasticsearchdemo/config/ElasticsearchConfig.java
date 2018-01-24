91892edfdbddbdf5f1ee63a82a916f3398bb56b2
byejude <edelweissasuare@163.com>
2018-01-24 12:36:10 +0800

    fix bugs

--- src/main/java/com/tulip/springbootelasticsearchdemo/config/ElasticsearchConfig.java
+++ src/main/java/com/tulip/springbootelasticsearchdemo/config/ElasticsearchConfig.java
@@ -3,10 +3,11 @@ package com.tulip.springbootelasticsearchdemo.config;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
+//import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -14,25 +15,27 @@ import java.net.UnknownHostException;
 @Configuration
 public class ElasticsearchConfig {
 
-    @Value("${ealsticsearch.cluster.name}")
+    @Value("${elasticsearch.cluster.name}")
     private String clusterName;
 
     @Value("${elasticsearch.host}")
     private String host;
 
+    private TransportClient transportClient2;
+
     @Bean
     public TransportClient transportClient() throws UnknownHostException{
         //set  cluster name
-        Settings settings = Settings.builder().put("cluster.name")
+        Settings settings = Settings.builder().put("cluster.name",clusterName)
                 .build();
-        TransportClient transportClient = new PreBuiltTransportClient(settings);
+         transportClient2 = new PreBuiltTransportClient(settings);
         //set nodes
         String[] nodes = host.split(",");
         for (String node:nodes
              ) {
             if(node.length()>0){
                 String[] hostPort = node.split(":");
-                transportClient.addTransportAddress(
+                transportClient2.addTransportAddress(
                         new InetSocketTransportAddress(
                                 InetAddress.getByName(hostPort[0]),
                                 Integer.parseInt(hostPort[1])
@@ -40,6 +43,6 @@ public class ElasticsearchConfig {
                 );
             }
         }
-        return transportClient;
+        return transportClient2;
     }
 }
